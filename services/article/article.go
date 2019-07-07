9aefeac966bff390cb548d1f3c84bd3d7bf3acc4
daniilperestoronin <perestoronin.daniil@gmail.com>
2019-07-07 23:03:38 +0300

    Add dummy article

---
+++ services/article/article.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	response, err := http.Get("https://newsapi.org/v2/top-headlines?sources=associated-press&apiKey=API_KEY")
+
+	if err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(responseData))
+}
