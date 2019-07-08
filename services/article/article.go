7a8a62a727afa852bdd0bc91bd82c1e2d5e2eb28
daniilperestoronin <perestoronin.daniil@gmail.com>
2019-07-08 21:50:22 +0300

    Update article

--- services/article/article.go
+++ services/article/article.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/gin-gonic/gin"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -9,16 +10,28 @@ import (
 )
 
 func main() {
-	response, err := http.Get("https://newsapi.org/v2/top-headlines?sources=associated-press&apiKey=API_KEY")
 
-	if err != nil {
-		fmt.Print(err.Error())
-		os.Exit(1)
-	}
+	apiKey := os.Args[1]
 
-	responseData, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(string(responseData))
+	route := gin.Default()
+	route.GET("/", func(context *gin.Context) {
+
+		sources := context.Query("sources")
+
+		response, err := http.Get("https://newsapi.org/v2/top-headlines?sources=" + sources + "&apiKey=" + apiKey)
+
+		if err != nil {
+			fmt.Print(err.Error())
+			os.Exit(1)
+		}
+
+		responseData, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		context.Header("Content-Type", "application/json; charset=utf-8")
+		context.String(200, string(responseData))
+	})
+	route.Run(":8082")
 }
