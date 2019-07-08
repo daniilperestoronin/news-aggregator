30869036331a860a35e3c0e943e354ca5e7cf51b
daniilperestoronin <perestoronin.daniil@gmail.com>
2019-07-08 21:50:42 +0300

    Update source

--- services/source/source.go
+++ services/source/source.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/gin-gonic/gin"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -9,16 +10,22 @@ import (
 )
 
 func main() {
-	response, err := http.Get("https://newsapi.org/v1/sources?language=en")
+	router := gin.Default()
+	router.GET("/", func(context *gin.Context) {
+		response, err := http.Get("https://newsapi.org/v1/sources?language=en")
 
-	if err != nil {
-		fmt.Print(err.Error())
-		os.Exit(1)
-	}
+		if err != nil {
+			fmt.Print(err.Error())
+			os.Exit(1)
+		}
 
-	responseData, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(string(responseData))
+		responseData, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		context.Header("Content-Type", "application/json; charset=utf-8")
+		context.String(200, string(responseData))
+	})
+	router.Run("8081")
 }
