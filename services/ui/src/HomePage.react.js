f85104b84736a31017caf22c95e771d88d186b34
daniilperestoronin <perestoronin.daniil@gmail.com>
2019-07-09 22:12:45 +0300

    Add handling of changing source

--- services/ui/src/HomePage.react.js
+++ services/ui/src/HomePage.react.js
@@ -9,56 +9,61 @@ import axios from 'axios';
 
 export default class Home extends React.Component {
 
-    state = {
-        sources: [],
-        articles: []
-    };
+  state = {
+    sources: [],
+    articles: [],
+    sourceKey: ''
+  };
 
-    componentDidMount() {
-        axios.get('http://localhost:8081/')
-            .then(res => {
-                const sources = res.data.sources;
-                this.setState({sources});
-            })
+  componentDidMount() {
+    axios.get('http://localhost:8081/')
+    .then(res => {
+      const sources = res.data.sources;
+      this.setState({sources});
+    });
+  }
 
-        axios.get('http://localhost:8082/?sources=techcrunch')
-            .then(res => {
-                const articles = res.data.articles;
-                this.setState({articles});
-            })
-    }
+  getArticlesForSource = (event) => {
+    axios.get('http://localhost:8082/?sources='+event.target.value)
+    .then(res => {
+      const articles = res.data.articles;
+      this.setState({articles});
+    });
+  }
 
-    render() {
-        return (<SiteWrapper>
-                <Page.Content title="News Aggregator">
+  render() {
+    return (<SiteWrapper>
+          <Page.Content title="News Aggregator">
 
-                    <Form.Group label="Select source">
-                        <Form.Select>
-                            {this.state.sources.map((item) => {
-                                    return (
-                                        <option key={item.id}>{item.name}</option>
-                                    );
-                                }
-                            )}
-                        </Form.Select>
-                    </Form.Group>
+            <Form.Group label="Select source">
+              <Form.Select
+                  onChange={this.getArticlesForSource}
+              >
+                {this.state.sources.map((item) => {
+                      return (
+                          <option key={item.id} value={item.id}>{item.name}</option>
+                      );
+                    }
+                )}
+              </Form.Select>
+            </Form.Group>
 
-                    <Grid.Row cards={true}>
-                        {this.state.articles.map((item) => {
-                                return (
-                                    <Card
-                                        title={item.title}
-                                        isCollapsible
-                                        body={item.description}
-                                        footer={item.author}
-                                    />
-                                );
-                            }
-                        )}
+            <Grid.Row cards={true}>
+              {this.state.articles.map((item) => {
+                    return (
+                        <Card
+                            title={item.title}
+                            isCollapsible
+                            body={item.description}
+                            footer={item.author}
+                        />
+                    );
+                  }
+              )}
 
-                    </Grid.Row>
-                </Page.Content>
-            </SiteWrapper>
-        )
-    };
+            </Grid.Row>
+          </Page.Content>
+        </SiteWrapper>
+    )
+  };
 }
