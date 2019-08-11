aab81480d7aa6d5cab0e7d468f18c84eb75b5812
daniilperestoronin <perestoronin.daniil@gmail.com>
2019-08-11 23:33:21 +0300

    Update terraform

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -27,3 +27,59 @@ module "gke" {
 
 data "google_client_config" "default" {
 }
+
+data "template_file" "proxy_files" {
+  template = "${file("../kubernetes/proxy/<.yaml>")}"
+}
+
+resource "null_resource" "proxy_deployment" {
+  triggers = {
+    manifest_sha1 = "${sha1("${data.template_file.proxy_files.rendered}")}"
+  }
+
+  provisioner "local-exec" {
+    command = "kubectl create -f -<<EOF\n${data.template_file.proxy_files.rendered}\nEOF"
+  }
+}
+
+data "template_file" "service_files" {
+  template = "${file("../kubernetes/service/templates/<.yaml>")}"
+}
+
+resource "null_resource" "service_deployment" {
+  triggers = {
+    manifest_sha1 = "${sha1("${data.template_file.service_files.rendered}")}"
+  }
+
+  provisioner "local-exec" {
+    command = "kubectl create -f -<<EOF\n${data.template_file.service_files.rendered}\nEOF"
+  }
+}
+
+data "template_file" "monitoring_files" {
+  template = "${file("../kubernetes/monitoring/<.yaml>")}"
+}
+
+resource "null_resource" "monitoring_deployment" {
+  triggers = {
+    manifest_sha1 = "${sha1("${data.template_file.monitoring_files.rendered}")}"
+  }
+
+  provisioner "local-exec" {
+    command = "kubectl create -f -<<EOF\n${data.template_file.monitoring_files.rendered}\nEOF"
+  }
+}
+
+data "template_file" "logging_files" {
+  template = "${file("../kubernetes/logging/<.yaml>")}"
+}
+
+resource "null_resource" "monitoring_deployment" {
+  triggers = {
+    manifest_sha1 = "${sha1("${data.template_file.logging_files.rendered}")}"
+  }
+
+  provisioner "local-exec" {
+    command = "kubectl create -f -<<EOF\n${data.template_file.logging_files.rendered}\nEOF"
+  }
+}
